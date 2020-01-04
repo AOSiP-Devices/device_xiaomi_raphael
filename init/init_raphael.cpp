ec835ea1f6a0bf29a34e433090ead24bdcbca5c2
Nauval Rizky <enuma.alrizky@gmail.com>
2020-01-05 05:57:10 +0900

    raphael: libinit: set dalvik heap configuration from here
    * Will covers up 6,8, and 12GB version of raphael
    * values are taken from frameworks/native/build
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -18,6 +18,7 @@
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
 
 #include "property_service.h"
 #include "vendor_init.h"
@@ -59,6 +60,28 @@ void load_raphael() {
 }
 
 
+void load_dalvikvm_properties()
+{
+    struct sysinfo sys;
+
+    sysinfo(&sys);
+    if (sys.totalram < 7000ull * 1024 * 1024) {
+        // 4/6GB RAM
+        property_set("dalvik.vm.heapstartsize", "16m");
+        property_set("dalvik.vm.heaptargetutilization", "0.5");
+        property_set("dalvik.vm.heapmaxfree", "32m");
+    } else {
+        // 8/12/16GB RAM
+        property_set("dalvik.vm.heapstartsize", "24m");
+        property_set("dalvik.vm.heaptargetutilization", "0.46");
+        property_set("dalvik.vm.heapmaxfree", "48m");
+    }
+
+    property_set("dalvik.vm.heapgrowthlimit", "256m");
+    property_set("dalvik.vm.heapsize", "512m");
+    property_set("dalvik.vm.heapminfree", "8m");
+}
+
 void vendor_load_properties() {
     std::string region = android::base::GetProperty("ro.boot.hwc", "");
 
@@ -77,4 +100,6 @@ void vendor_load_properties() {
     property_override("persist.bluetooth.bluetooth_audio_hal.disabled", "true");
     property_override("persist.vendor.qcom.bluetooth.enable.splita2dp", "false");
     property_override("vendor.audio.feature.a2dp_offload.enable", "false");
+    load_dalvikvm_properties();
+
 }
