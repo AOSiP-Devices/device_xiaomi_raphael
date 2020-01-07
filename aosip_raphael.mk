e3aed96880d4bf72fddbdf79bd30864e7d680099
Akhil Narang <akhilnarang.1999@gmail.com>
2020-01-07 18:54:16 +0530

    raphael: sanitize
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- aosip_raphael.mk
+++ aosip_raphael.mk
@@ -18,4 +18,3 @@ PRODUCT_NAME := aosip_raphael
 BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
