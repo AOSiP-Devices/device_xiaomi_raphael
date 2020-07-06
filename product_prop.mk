8bd558c984cc3c13d5c9b0fa75bc0cb7c788d7a9
Akash <akashniki@gmail.com>
2020-07-06 23:07:52 +0200

    raphael: Enable fluence voice recording
    This fixes google voice recorder
    
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>
    Change-Id: I27e302f73aa245ee5c3fe90338aaf606de464a69

--- product_prop.mk
+++ product_prop.mk
@@ -15,4 +15,5 @@ endif
 
 # Media
 PRODUCT_PRODUCT_PROPERTIES += \
-    debug.stagefright.omx_default_rank.sw-audio=16
+    debug.stagefright.omx_default_rank.sw-audio=16 \
+    persist.vendor.audio.fluence.voicerec=true
