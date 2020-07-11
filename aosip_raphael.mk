011d789cb9e9c90b0e8f5999b2b7bc89cdb87360
Abdulwahab Isam <abdoi94.iq@gmail.com>
2020-07-11 10:54:20 +0300

    raphael: enable updating of APEXes
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>
    Change-Id: Ic1469103425e5345f0db88e40e93fd600907e5c9

--- aosip_raphael.mk
+++ aosip_raphael.mk
@@ -9,6 +9,9 @@ $(call inherit-product, device/xiaomi/raphael/device.mk)
 # Inherit some common AOSiP stuff.
 $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
+# Inherit PSU
+$(call inherit-product-if-exists, vendor/google/psu/google-psu.mk)
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := raphael
@@ -18,3 +21,5 @@ PRODUCT_NAME := aosip_raphael
 BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+
